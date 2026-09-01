e489d4d5524b4aef585481468508253d246ccb4b
jfontaine-ehe <jfontaine@eheinc.com>
2026-09-01 15:28:14 -0400

    updated pfas modal with error messages

--- clientUpdates/comb_data_3mdtb/views.py
+++ clientUpdates/comb_data_3mdtb/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .utils import process_pfas, process_annual_flow, get_dashboard_data
+from .forms import PFASUpdateForm
 
 from django.views.decorators.cache import never_cache
 
@@ -21,4 +22,23 @@ def landing_page(request):
 @login_required
 @never_cache
 def pfas_update(request):
-    return {}
+    if request.method == "POST":
+        form = PFASUpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Handle valid form (save to database)
+            form.save()
+            return redirect("comb_data_3mdtb:landing_page")
+        else:
+            # Re-render landing page with form errors
+            pwsid = request.user.username
+            data = get_dashboard_data(pwsid)
+            return render(
+                request,
+                "comb_data_3mdtb/landing_page.html",
+                {
+                    "data": data,
+                    "form": form,
+                    "show_modal": True
+                }
+            )
+    return redirect("comb_data_3mdtb:landing_page")
