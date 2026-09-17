2b0c3974cf6eed7db820a7feaba41c75aca334c0
jfontaine-ehe <jfontaine@eheinc.com>
2026-09-17 10:57:22 -0400

    undo: add purchased water option for source_type

--- clientUpdates/comb_data_3mdtb/forms.py
+++ clientUpdates/comb_data_3mdtb/forms.py
@@ -34,7 +34,6 @@ class AddNewSourceForm(forms.ModelForm):
                 ("Groundwater Well", "Groundwater Well"),
                 ("Surface Water", "Surface Water"),
                 ("Interconnection", "Interconnection"),
-                ("Purchased Water", "Purchased Water"),
                 ("Other", "Other"),
             ]),
             "source_other": forms.TextInput(attrs={"class": "pfas-form-control", "placeholder": "Please specify other source type"}),
